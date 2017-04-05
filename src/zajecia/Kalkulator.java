9ed2bc390ca5e4b2c9bad8ef1a74d93a1c84aef9
SzkolenieMS <lykamspam@kozacki.pl>
2017-04-05 19:27:23 +0200

    added calc

--- src/zajecia/Kalkulator.java
+++ src/zajecia/Kalkulator.java
@@ -1,5 +1,7 @@
 package zajecia;
 
+import com.sun.org.apache.xpath.internal.SourceTree;
+
 import java.util.Scanner;
 
 /**
@@ -11,7 +13,69 @@ public class Kalkulator {
 
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("give me number:");
+        int i =0;
+        while (i ==0) {
+            System.out.println("Choose one number:");
+            System.out.println("1. + ");
+            System.out.println("2. - ");
+            System.out.println("3. * ");
+            System.out.println("4. / ");
+            System.out.println("5. Exit");
+            System.out.println();
+            int nachose = scanner.nextInt();
+            if (nachose > 0 && nachose < 6) {
+                if (nachose == 1) {
+                    System.out.print("First number: ");
+                    int element1 = scanner.nextInt();
+                    System.out.println();
+                    System.out.print("Second number: ");
+                    int element2 = scanner.nextInt();
+                    System.out.println();
+                    System.out.println("Suma: " + (element1 + element2));
+                } else if (nachose == 2) {
+                    System.out.print("First number: ");
+                    int element1 = scanner.nextInt();
+                    System.out.println();
+                    System.out.print("Second number: ");
+                    int element2 = scanner.nextInt();
+                    System.out.println();
+                    if (element1 > element2) {
+                        System.out.println("Roznica: " + (element1 - element2));
+                    } else {
+                        System.out.println("Roznica: " + (element2 - element1));
+                    }
+
+                } else if (nachose == 3) {
+                    System.out.print("First number: ");
+                    int element1 = scanner.nextInt();
+                    System.out.println();
+                    System.out.print("Second number: ");
+                    int element2 = scanner.nextInt();
+                    System.out.println();
+                    System.out.println("Iloczyn: " + (element1 * element2));
+                } else if (nachose == 4) {
+                    System.out.print("First number: ");
+                    int element1 = scanner.nextInt();
+                    System.out.println();
+
+                    int element2 = scanner.nextInt();
+                    System.out.println();
+                    if (element2 == 0) {
+                        System.out.print("Nie dziel przez 0: ");
+                    } else {
+                        System.out.println("Iloraz: " + (element1 / (double) element2));
+                    }
+                } else if (nachose ==5){
+                    i =1;
+                }
+
+            } else {
+
+                System.out.println("Numbers from 1-5");
+
+            }
+
+        }
 
 
 
