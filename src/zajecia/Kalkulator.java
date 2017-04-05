db44cc8a33aa0948845aade99514f2838d5cabc9
SzkolenieMS <lykamspam@kozacki.pl>
2017-04-05 19:59:48 +0200

    added abs and first func

--- src/zajecia/Kalkulator.java
+++ src/zajecia/Kalkulator.java
@@ -13,17 +13,18 @@ public class Kalkulator {
 
 
         Scanner scanner = new Scanner(System.in);
-        int i =0;
-        while (i ==0) {
+        int i = 0;
+        while (i == 0) {
             System.out.println("Choose one number:");
             System.out.println("1. + ");
             System.out.println("2. - ");
             System.out.println("3. * ");
             System.out.println("4. / ");
             System.out.println("5. Exit");
+            System.out.println("6. Wartosc bezwzgledna z roznicy"); 
             System.out.println();
             int nachose = scanner.nextInt();
-            if (nachose > 0 && nachose < 6) {
+            if (nachose > 0 && nachose < 7) {
                 if (nachose == 1) {
                     System.out.print("First number: ");
                     int element1 = scanner.nextInt();
@@ -39,12 +40,7 @@ public class Kalkulator {
                     System.out.print("Second number: ");
                     int element2 = scanner.nextInt();
                     System.out.println();
-                    if (element1 > element2) {
-                        System.out.println("Roznica: " + (element1 - element2));
-                    } else {
-                        System.out.println("Roznica: " + (element2 - element1));
-                    }
-
+                    System.out.println("Roznica: " + (element1 - element2));
                 } else if (nachose == 3) {
                     System.out.print("First number: ");
                     int element1 = scanner.nextInt();
@@ -57,7 +53,6 @@ public class Kalkulator {
                     System.out.print("First number: ");
                     int element1 = scanner.nextInt();
                     System.out.println();
-
                     int element2 = scanner.nextInt();
                     System.out.println();
                     if (element2 == 0) {
@@ -65,19 +60,30 @@ public class Kalkulator {
                     } else {
                         System.out.println("Iloraz: " + (element1 / (double) element2));
                     }
-                } else if (nachose ==5){
-                    i =1;
+                } else if (nachose == 5) {
+                    i = 1;
+                } else if (nachose == 6) {
+                    System.out.print("First number: ");
+                    int element1 = scanner.nextInt();
+                    System.out.println();
+                    System.out.print("Second number: ");
+                    int element2 = scanner.nextInt();
+                    System.out.println();
+                    int roznica = abs(element1, element2);
+                    System.out.println("Wartosc bezwzglednia = " + roznica);
+                } else {
+                    System.out.println("Numbers from 1-5");
                 }
-
-            } else {
-
-                System.out.println("Numbers from 1-5");
-
             }
-
         }
-
-
-
     }
+
+
+    public static int abs(int number1, int number2) {
+        int roznica = number1 - number2;
+        if (roznica < 0) roznica *= -1;
+        return roznica;
+    }
+
+
 }
