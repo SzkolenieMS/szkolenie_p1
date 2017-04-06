d3c20ae5f1968cb0205098bb45170df35e67f76c
SzkolenieMS <lykamspam@kozacki.pl>
2017-04-06 19:03:39 +0200

    update

--- src/zajecia/Zajecia3.java
+++ src/zajecia/Zajecia3.java
@@ -8,21 +8,41 @@ import java.util.Scanner;
 public class Zajecia3 {
     public static void main(String[] args) {
 
-        fahrenheittoCelsius(80);
-        celsiusToFarenheit(25);
-        minMax(3, 4, 9);
-
+//        fahrenheittoCelsius(80);
+//        celsiusToFarenheit(25);
+//        minMax(3, 4, 9);
+//
         Scanner scanner = new Scanner(System.in);
-        System.out.println("podaj a");
+
+        System.out.println("podaj wage");
         int a = scanner.nextInt();
-        System.out.println("podaj b");
-        int b = scanner.nextInt();
-        System.out.println("podaj c");
-        int c = scanner.nextInt();
-        minMax(a, b, c);
+        System.out.println("podaj wysokosc");
+        double b = scanner.nextDouble();
+        bmi(a,b);
+//        System.out.println("podaj c");
+//        int c = scanner.nextInt();
+//        minMax(a, b, c);
 
 
     }
+
+    public static void bmi (int weight, double height){
+//        waga/wzrost*wzrost
+        double bmi = weight/(height*height);
+        System.out.println("Your bmi" + bmi);
+        if (bmi <= 18.5) {
+            System.out.println("Masz niedowage");
+
+        } else if (bmi > 18.5 && bmi <24.9) {
+            System.out.println("Idealna waga");
+        } else {
+            System.out.println("masz nadwage");
+        }
+
+
+    }
+
+
 
     public static void fahrenheittoCelsius(int fahrenheit) {
         double celsjusz = (fahrenheit - 32) / 1.8;
