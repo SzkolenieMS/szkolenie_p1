75e00efbb0fabdc8c9aadb1c60540288e1712d4e
SzkolenieMS <lykamspam@kozacki.pl>
2017-04-11 18:15:37 +0200

    zajecia 5

--- src/zajecia/ZajecieCztery.java
+++ src/zajecia/ZajecieCztery.java
@@ -1,5 +1,6 @@
 package zajecia;
 
+import java.util.Random;
 import java.util.Scanner;
 
 /**
@@ -8,51 +9,92 @@ import java.util.Scanner;
 public class ZajecieCztery {
 
     public static void main(String[] args) {
-        int[] tablica = getArrayFromUser();
+
+
+//        int[] tablica = getArrayFromUser();
 //        minFromArray(tablica);
 //        maxFromArray(tablica);
 //        avg(tablica);
 //         revers(tablica);
 //        statistic(tablica);
-        displayArray(tablica);
+//        displayArray(tablica);
+//        homework();
 
     }
-public static void displayReversed(int[] array){
+
+    public static void lineOfNumbers(int value){
+
+        
+
+
+    }
+
+
+    public static int[] getRandomArray(int size) {
+        return getRandomArray(size, Integer.MAX_VALUE);
+    }
+
+    public static int[] getRandomArray(int size, int bound) {
+        return getRandomArray(size, bound, 0);
+    }
+
+    public static int[] getRandomArray(int size, int bound, int offset) {
+
+        Random random = new Random();
+        int[] array = new int[size];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(bound) + offset;
+        }
+        return array;
+    }
+
+    public static void homework() {
+        Random random = new Random();
+        int value = random.nextInt(10) + 15;
+//        System.out.println(value);
+        int[] tablica = getArray(value);
+        statistic(tablica);
+
+        // wywolac statisic
+        //dla wylosowanej liczby -12 zrobi drzewko
+    }
+
+
+    public static void displayReversed(int[] array) {
         displayArray(reversTable(array));
 
 
-}
+    }
 
-    public static void displayArray(int[] array){
+    public static void displayArray(int[] array) {
         System.out.println("[");
-        for (int i = 0; i <array.length ; i++) {
+        for (int i = 0; i < array.length; i++) {
             System.out.print(array[i]);
-            if (i != array.length -1)
+            if (i != array.length - 1)
                 System.out.println(", ");
         }
         System.out.println("]");
 
 
+    }
+
+
+    public static int[] getArrayFromUser() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Podaj ilosc elementow: ");
+        int ile = scanner.nextInt();
+        int[] array = new int[ile];
+        for (int i = 0; i < ile; i++) {
+            System.out.println("Podaj " + (i + 1) + " liczbe");
+            array[i] = scanner.nextInt();
+
+        }
+        return array;
 
     }
 
 
-    public static int[] getArrayFromUser(){
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Podaj ilosc elementow: ");
-        int ile = scanner.nextInt();
-        int[] array = new int[ile] ;
-        for (int i = 0; i < ile ; i++) {
-            System.out.println("Podaj " + (i+1) + " liczbe");
-            array[i] = scanner.nextInt();
-
-            }
-        return array;
-
-        }
-
-
-    public static void statistic (int[] array){
+    public static void statistic(int[] array) {
         System.out.println("Minimalna: " + minFromArray(array));
         System.out.println("Maksymalna: " + maxFromArray(array));
         System.out.println("Suma: " + sum(array));
@@ -63,9 +105,9 @@ public static void displayReversed(int[] array){
 
     }
 
-    public static void revers(int[] array){
+    public static void revers(int[] array) {
         System.out.print("[");
-        for (int i = array.length-1; i >= 0 ; i--) {
+        for (int i = array.length - 1; i >= 0; i--) {
             System.out.print(array[i]);
             if (i > 0) {
                 System.out.print(", ");
@@ -77,25 +119,25 @@ public static void displayReversed(int[] array){
 
     }
 
-    public static int[] reversTable(int[] array){
-        int temp =0;
-        for (int i = 0; i < (array.length/2)+1 ; i++) {
+    public static int[] reversTable(int[] array) {
+        int temp = 0;
+        for (int i = 0; i < (array.length / 2) + 1; i++) {
 //            System.out.println(i);
-            temp = array[0+i];
-             array[0+i] = array[array.length-i-1];
-            array[array.length-i-1] = temp;
+            temp = array[0 + i];
+            array[0 + i] = array[array.length - i - 1];
+            array[array.length - i - 1] = temp;
         }
         return array;
 
     }
 
-        public static int abs(int number) {
-        if (number <0){
-            number = number *-1; //number *= -1
+    public static int abs(int number) {
+        if (number < 0) {
+            number = number * -1; //number *= -1
         }
         return number;
 
-        }
+    }
 
     public static int sum(int[] array) {
         int sum = 0;
@@ -107,12 +149,12 @@ public static void displayReversed(int[] array){
 
 
     public static double avg(int[] array) {
-        return sum(array)/(double)array.length;
+        return sum(array) / (double) array.length;
     }
 
 
     public static int span(int[] array) {
-        return maxFromArray(array) -minFromArray(array);
+        return maxFromArray(array) - minFromArray(array);
     }
 
 
@@ -124,7 +166,7 @@ public static void displayReversed(int[] array){
         }
 
 //        System.out.println("Najmniejsza wartosc: " + najmniejszy);
-            return najmniejszy;
+        return najmniejszy;
     }
 
 
@@ -136,7 +178,7 @@ public static void displayReversed(int[] array){
         }
 
 //        System.out.println("Najmniejsza wartosc: " + najwiekszy);
-    return najwiekszy;
+        return najwiekszy;
 
 
     }
@@ -155,4 +197,17 @@ public static void displayReversed(int[] array){
 
 
     }
+
+    public static int[] getArray(int value) {
+        int size = value / 2;
+        int[] array = new int[size];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = 2 * i + value % 2;
+
+        }
+        return array;
+
+    }
+
+
 }
