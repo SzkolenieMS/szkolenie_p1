e60983abcef9c6101e5d04165017d39db3210b3f
SzkolenieMS <lykamspam@kozacki.pl>
2017-04-12 13:01:17 +0200

    8.29 na 1mln wykonan

--- src/zajecia/Zajecia6.java
+++ src/zajecia/Zajecia6.java
@@ -23,100 +23,88 @@ public class Zajecia6 {
 //        int zmienna = random.nextInt(100);
 //        System.out.println("Kroki: " +   gameBotMoreLess(zmienna) );
 //        System.out.println(zmienna);
-
-        System.out.println("Srednia ilosc krokow na sto gier = " +stoGier()/10000);
+        GrajacyBot(1000000);
     }
-    //30
-    //7
-
-    public static int stoGier(){
-int suma = 0;
-int krokow = 0;
-        for (int i = 0; i < 10000; i++) {
-
-
+    public static void GrajacyBot(int ilegier) {
+        int suma = 0;
+        int krokow;
+        for (int i = 0; i < ilegier; i++) {
             Random random = new Random();
             int zmienna = random.nextInt(100);
             krokow = gameBotMoreLess(zmienna);
             suma += krokow;
         }
-        return suma;
+
+            System.out.println("Srednia ilosc krokow na " + ilegier + " gier = " + (double)suma / ilegier);
     }
 
 
-public static int[] BOT(int number, int lastnumber, int mniejszanajwieksza, int[] tablicawynikow, int kroki){
+    public static int[] BOT(int result, int lastnumber, int mniejszanajwieksza, int[] tablicawynikow, int kroki, int wiekszamniejsza) {
         Random random = new Random();
-        int randomik = 0;
-        int[] tablica ={0,0};
-        int j = 0;
+        int[] tablica = {0, 0, 0};
         boolean truefalse = true;
-       // if (lastnumber < 0) lastnumber *= -1;
-       // if (lastnumber < 2) lastnumber+=20;
-    while (truefalse){
-//        j++;
-        truefalse = false;
-        if (number == 0 ){
-            tablica[0] = random.nextInt(mniejszanajwieksza-lastnumber)+lastnumber;
-            tablica[1]= mniejszanajwieksza;
-//            System.out.println("Randomik bo za male: " + tablica[0] + " last " + lastnumber + " mniej " + mniejszanajwieksza);
 
+        while (truefalse) {
+            truefalse = false;
+            if (result == 0) {
+                tablica[0] = random.nextInt(mniejszanajwieksza - lastnumber) + lastnumber;
+//                System.out.println("mniejsze( "+ mniejszanajwieksza+  " - " + lastnumber + " ) +" + lastnumber);
+//                if (lastnumber >= wiekszamniejsza)
+                    tablica[2] = lastnumber;
 
-        }else
-    {
-        if (lastnumber < mniejszanajwieksza) tablica[1]= lastnumber;
-        tablica[0] = random.nextInt(lastnumber);
-                //+(100-lastnumber);
+                tablica[1] = mniejszanajwieksza;
 
-//        System.out.println("Randomik bo za duze: " + tablica[0]);
+            } else {
+//                System.out.println(wiekszamniejsza);
+                if ((mniejszanajwieksza-wiekszamniejsza)==0) mniejszanajwieksza+=1;
+                tablica[0] = random.nextInt(mniejszanajwieksza-wiekszamniejsza)+wiekszamniejsza;//lastnumber)-;
+//                System.out.println("wieksze(" + wiekszamniejsza + ")");
+//                if (lastnumber <= mniejszanajwieksza)
+                    tablica[1] = lastnumber;
+                tablica[2] = wiekszamniejsza;
 
-    }
-        for (int i = 0; i <tablicawynikow.length-1 ; i++) {
-            if (tablicawynikow[i] == tablica[0] && tablica[0] != 0) {
-                truefalse = true;
+            }
+            for (int i = 0; i < tablicawynikow.length - 1; i++) {
+                if (tablicawynikow[i] == tablica[0] && tablica[0] != 0) {
+                    truefalse = true;
+                }
             }
         }
-//    if (j>20) break;
+//        if (tablica[1] == 0) tablica[1] =1;
+//        System.out.println("Random: " + tablica[0] + " Mniejszawieksza: " + tablica[1] + " Wiekszamniejsza: " + tablica[2]);
+        tablicawynikow[kroki] = tablica[0];
+        return tablica;
+
+
     }
 
-    tablicawynikow[kroki] = tablica[0];
-    return tablica;
-//    System.out.println(lastnumber + "last");
-
-
-
-}
-    public static int gameBotMoreLess(int number){
-
-//        System.out.println("Moj number" + number);
+    public static int gameBotMoreLess(int number) {
+//        System.out.println("Number to search: " + number);
+        Random random = new Random();
         int kroki = 0;
-        int lastnumber = 1;
-        int usernumber = -1;
-        int[] tablicawynikow = new int[50];
-        int[] tabelka ;
+        int usernumber = random.nextInt(100);
+        int lastnumber = usernumber;
+        int[] tablicawynikow = new int[80];
+        int[] tabelka;
         int result = 0;
-        int mniejszanajwieksza = 100+1;
-        while (number != usernumber){
-            tabelka = BOT(result, lastnumber, mniejszanajwieksza,tablicawynikow, kroki);
+        int mniejszanajwieksza = 100;
+        int wiekszamniejsza = 0;
+        while (number != usernumber) {
+            if (usernumber > number) result = 1;
+            if (usernumber < number) result = 0;
+            tabelka = BOT(result, lastnumber, mniejszanajwieksza, tablicawynikow, kroki , wiekszamniejsza);
             usernumber = tabelka[0];
             lastnumber = usernumber;
             mniejszanajwieksza = tabelka[1];
-//            System.out.println("shot:" + usernumber);
-            //if (usernumber > number) lastnumber;
+            wiekszamniejsza = tabelka[2];
             kroki++;
-            if (usernumber > number) result = 1;
-            if (usernumber < number) result = 0;
-            if (usernumber>1001 || usernumber<-1000) break;
-            if (kroki> 100) break;
         }
+//        System.out.println("Kroki: " + kroki);
         return kroki;
     }
 
 
-
-
-
-
-    public static void gamerMoreLessInitiate(){
+    public static void gamerMoreLessInitiate() {
 
         Random random = new Random();
         int liczbakrokow = gameMoreLess(random.nextInt(100));
@@ -124,13 +112,14 @@ public static int[] BOT(int number, int lastnumber, int mniejszanajwieksza, int[
 
 
     }
-    public static int gameMoreLess(int number){
-       //number 0-100
+
+    public static int gameMoreLess(int number) {
+        //number 0-100
         System.out.println("Moj number" + number);
         int kroki = 0;
         int usernumber = -1;
-        Scanner scanner= new Scanner(System.in);
-        while (number != usernumber){
+        Scanner scanner = new Scanner(System.in);
+        while (number != usernumber) {
             System.out.println("Podaj liczbe");
             usernumber = scanner.nextInt();
             if (usernumber > number) System.out.println("Za duza liczba");
@@ -143,51 +132,49 @@ public static int[] BOT(int number, int lastnumber, int mniejszanajwieksza, int[
     }
 
 
-
-    public static void runAvgUntilMethod(){
+    public static void runAvgUntilMethod() {
 
         Random random = new Random();
-        int [] array = Zajecia5.getRandomlowArray(random.nextInt(10)+15);
+        int[] array = Zajecia5.getRandomlowArray(random.nextInt(10) + 15);
         ZajecieCztery.displayArray(array);
 //        int suma = random.nextInt(1)+10;
 //
-        int suma = random.nextInt(85)+15;
+        int suma = random.nextInt(85) + 15;
 
         System.out.println("Liczba graniczna: " + suma);
-        System.out.println(avgUntil(array,suma));
-
+        System.out.println(avgUntil(array, suma));
 
 
     }
 
-    public static boolean avgUntil(int[] array, int sum){
+    public static boolean avgUntil(int[] array, int sum) {
         int arraysum = array[0];
-        int i=1;
+        int i = 1;
         int avg = 0;
-        while(avg < sum && array.length > i){
+        while (avg < sum && array.length > i) {
             arraysum += array[i];
-            avg = arraysum / i ;
+            avg = arraysum / i;
             i++;
         }
-        System.out.println("Average: "+ avg);
-        return avg>=sum;
+        System.out.println("Average: " + avg);
+        return avg >= sum;
 
     }
 
-    public static void printPowersOf2(int number){
-     int value = 1;
-        while(value < number){
+    public static void printPowersOf2(int number) {
+        int value = 1;
+        while (value < number) {
             System.out.println(value);
             value *= 2;
 
         }
     }
 
-    public static boolean sumUntil(int[] array, int sum){
+    public static boolean sumUntil(int[] array, int sum) {
         int arraysum = 0;
-        int i=0;
-        while(arraysum < sum){
-            if (array.length == i){
+        int i = 0;
+        while (arraysum < sum) {
+            if (array.length == i) {
                 return false;
             }
             arraysum += array[i];
@@ -199,12 +186,11 @@ public static int[] BOT(int number, int lastnumber, int mniejszanajwieksza, int[
     }
 
 
-
-    public static void rememberWhile(){
+    public static void rememberWhile() {
         int number = 10;
         int counter = 0;
         Random random = new Random();
-        while (number>0) {
+        while (number > 0) {
             counter++;
             number -= random.nextInt(3);
         }
