355cb61b7103f76cb0f39bd24e8587d0cfdf831a
SzkolenieMS <lykamspam@kozacki.pl>
2017-04-25 21:01:37 +0200

    quick change of result - homework

--- src/zajecia/oop/Quiz/result/MockResultsRepository.java
+++ src/zajecia/oop/Quiz/result/MockResultsRepository.java
@@ -4,17 +4,32 @@ package zajecia.oop.Quiz.result;
  * Created by RENT on 2017-04-25.
  */
 public class MockResultsRepository {
+
+    private Result[]results;
+
+
+    public MockResultsRepository(Result[] results) {
+
+        this.results = new Result[4];
+
+        results[0] = new Result("Maciej", 100);
+        results[1] = new Result("Jan",1);
+        results[2] = new Result("Adam", 100);
+        results[3] = new Result("Karol",1);
+
+    }
+
     public Result[] getAllResults(){
-        Result[] mockResults = new Result[4];
-
-        mockResults[0] = new Result("Maciej", 100);
-        mockResults[1] = new Result("Jan",1);
-        mockResults[2] = new Result("Adam", 100);
-        mockResults[3] = new Result("Karol",1);
-
-        return mockResults;
+        return results;
     }
 
 
+
+    public Result[] getTopResults (int n){
+    //1. 123123123
+    //2. 12321312
+    //3. 123123
+        return null;
+    }
 
 }
